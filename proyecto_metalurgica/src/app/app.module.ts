aab33ff8cc14a136455219f18718a88e382a6c6b
gonzalopereda52 <gonzalopereda52@gmail.com>
2023-11-12 02:28:54 -0300

    avances con la navbar

--- proyecto_metalurgica/src/app/app.module.ts
+++ proyecto_metalurgica/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { Routes } from '@angular/router';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+
+const routes: Routes = [
+  {path: "home" , component: HomeComponent},
+  {path: "contacto" , component: HomeComponent}
+
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
