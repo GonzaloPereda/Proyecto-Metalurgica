6115977c68de40ed83a5d49d55cdc7ebc8f70e29
unknown <gonzalopereda52@gmail.com>
2024-01-09 20:33:22 -0300

    solucionando mobile

--- proyecto_metalurgica/src/app/componentes/home/home.component.ts
+++ proyecto_metalurgica/src/app/componentes/home/home.component.ts
@@ -1,20 +1,43 @@
-import { Component, AfterViewInit  } from '@angular/core';
+import { Component, AfterViewInit , OnInit  } from '@angular/core';
 declare var $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 
 
 export class HomeComponent implements AfterViewInit {
-
-  ngAfterViewInit(): void {
-    $('#carouselExampleControls').carousel();
-  }
+  previousScrollY: number = 0;
+  constructor() { }
 
   ngOnInit(): void {
     window.scrollTo(0, 0);
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    const navbar = document.querySelector('.navbar');
+
+    if (navbar) {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > this.previousScrollY) {
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
+
+      this.previousScrollY = currentScrollY;
+    }
+  }
+
+  ngAfterViewInit(): void {
+    $('#carouselExampleControls').carousel();
   }
 
 }
