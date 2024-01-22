e66ce28ac650c1d0fe777af53e28327cda4a6d16
unknown <gonzalopereda52@gmail.com>
2024-01-22 00:04:22 -0300

    modificando flecha de carousel

--- proyecto_metalurgica/src/app/componentes/barandas/barandas.component.ts
+++ proyecto_metalurgica/src/app/componentes/barandas/barandas.component.ts
@@ -33,11 +33,11 @@ export class BarandasComponent implements OnInit{
   imagenesVisiblesH = this.imagenesH;
 
   ngOnInit() {
-    this.itemsPerSlide = window.innerWidth >= 768 ? 5 : 2;
+    this.itemsPerSlide = window.innerWidth >= 768 ? 5 : 3;
   }
 
   onResize(event: any) {
-    this.itemsPerSlide = event.target.innerWidth >= 768 ? 5 : 2;
+    this.itemsPerSlide = event.target.innerWidth >= 768 ? 5 : 3;
   }
 
   // cantidadVisibleH = 8; 
