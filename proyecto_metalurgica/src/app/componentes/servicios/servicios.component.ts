dfbb235ff5fed1d76f762789edd7d66e0e5bf023
unknown <gonzalopereda52@gmail.com>
2024-03-17 22:56:16 -0300

    imagenes nuevas

--- proyecto_metalurgica/src/app/componentes/servicios/servicios.component.ts
+++ proyecto_metalurgica/src/app/componentes/servicios/servicios.component.ts
@@ -21,21 +21,21 @@ export class ServiciosComponent implements OnInit{
   }
 
   imagenesCorte = [
-    'assets/cilindrados/1.jpg',
-    'assets/cilindrados/1.jpg',
-    'assets/cilindrados/1.jpg',
-    'assets/cilindrados/1.jpg',
-    'assets/cilindrados/1.jpg',
+    'assets/corte/1.jpg',
+    'assets/corte/2.jpg',
+    'assets/corte/3.jpg',
+    'assets/corte/4.jpg',
+    'assets/corte/1.jpg',
   ];
 
   imagenesVisiblesCorte = this.imagenesCorte;
 
   imagenesCilindrado = [
     'assets/cilindrados/1.jpg',
+    'assets/cilindrados/2.jpg',
+    'assets/cilindrados/3.jpg',
     'assets/cilindrados/1.jpg',
-    'assets/cilindrados/1.jpg',
-    'assets/cilindrados/1.jpg',
-    'assets/cilindrados/1.jpg',
+    'assets/cilindrados/2.jpg',
   ];
 
   imagenesVisiblesCilindrado = this.imagenesCilindrado;
